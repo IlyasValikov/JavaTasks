50adaad6de77b4a2ad82f9dcfcecebaa1b53129e
Ilya Valikov <valikov.skeleton1977@yandex.ru>
2023-06-06 19:00:25 +0300

    Change Search

--- src/main/java/ru/netologzy/tasks/Todos.java
+++ src/main/java/ru/netologzy/tasks/Todos.java
@@ -5,6 +5,7 @@ public class Todos {
     private Task[] tasks = new Task[0];
 
     private Task[] addToArray(Task[] current, Task task) {
+
         Task[] tmp = new Task[current.length + 1];
         for (int i = 0; i < current.length; i++) {
             tmp[i] = current[i];
@@ -25,6 +26,7 @@ public class Todos {
     }
 
     public Task[] search(String query) {
+
         Task[] result = new Task[0];
         for (Task task : tasks) {
             if (task.matches(query)) {
